c935edf704830d4ca9cb1506f7a38cc03cd68578
juankzero <juan.zero@unitec.edu>
2020-05-01 08:28:06 -0600

    Mi primer commit en proyecto de laboratorio

---
+++ src/Semana1/HolaMundo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Semana1;
+
+/**
+ *
+ * @author Juan Carlos Zepeda
+ */
+public class HolaMundo {
+    
+    public static void main(String[] args) 
+    {
+        
+        System.out.println("Hola Mundo ... Desde Lab. de Progra 2");
+        
+    }
+    
+}
